ad21c90c3f043a4a5b1641af3ab943f73db77a9b
Rasujon3 <rasujon3@gmail.com>
2022-04-03 02:58:49 +0600

    blog details done

--- src/Components/BlogDetails/BlogDetails.js
+++ src/Components/BlogDetails/BlogDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "./BlogDetails.css";
 import { BsChevronLeft } from "react-icons/bs";
@@ -6,6 +6,15 @@ import { BsChevronLeft } from "react-icons/bs";
 const BlogDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [blog, setBlog] = useState({});
+
+  useEffect(() => {
+    const url = `https://retro-tech-talks.herokuapp.com/getBlog/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBlog(data));
+  }, [id]);
+
   return (
     <>
       <div className="header-gradient" />
@@ -14,6 +23,13 @@ const BlogDetails = () => {
           <BsChevronLeft />
           <p>back</p>
         </button>
+        <div className="blog-details">
+          <div className="blog-image">
+            <img src={blog?.imageURL} alt="img" />
+          </div>
+          <h1>{blog?.title}</h1>
+          <p>{blog?.blog}</p>
+        </div>
       </div>
     </>
   );
